724be91bae9b24978dcdb7e52585cf1e014d2fe5
delcarlswlhs <delcarls@wlhs.wlwv.k12.or.us>
2023-04-19 09:38:56 -0700

    added factorial

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,51 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(factorial(3));
         System.out.println(factorial(10));
+        System.out.println(factorial(12));
+        System.out.println(SumOfNumbers(3));
+        RecursiveCountdown(10);
     }
 
+    public static void RecursiveCountdown(int n){
+        if(n==1){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(n);
+            RecursiveCountdown(n-1);
+        }
+
+    }
     public static int SumOfNumbers(int n){
-        return 0;
+        if(n==0){
+            return 0;
+        }
+        return n + SumOfNumbers(n-1);
+    }
+
+    public static int SumOfNumbers2(int n){
+        if(n != 0){
+            return n + SumOfNumbers2(n-1);
+        }
+        else{
+            return n;
+        }
     }
 
     public static int factorial(int n){
-        if(n==1){
+        if(n==0){
             return 1;
         }
         return n*factorial(n-1);
     }
+
+    public static int sumArray(int[] arrayIn, int n){
+        if(n == 0){
+            return 0;
+        }
+        else{
+            return arrayIn[n-1] + sumArray(arrayIn, n-1);
+        }
+    }
+
 }
