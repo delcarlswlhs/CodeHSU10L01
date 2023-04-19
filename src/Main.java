926a86fae2c6a53eee40b870e3eef9c3040440db
delcarlswlhs <delcarls@wlhs.wlwv.k12.or.us>
2023-04-19 09:02:47 -0700

    added factorial

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(factorial(3));
+        System.out.println(factorial(10));
     }
 
     public static int SumOfNumbers(int n){
         return 0;
+    }
+
+    public static int factorial(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*factorial(n-1);
     }
 }
